16629c7a282871a51bab7f349f461895c7df4b65
jottave <joao_victorflamengo10@outlook.com>
2024-05-17 12:25:57 -0300

    add contacts list

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { Contact } from '../../models/contact';
+import { ContactsService } from '../../services/contacts/contacts.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardModule, ButtonModule, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private contactsService: ContactsService) {}
 
+  contacts: Contact[] = [];
+
+  ngOnInit() {
+    this.contactsService.getContacts().subscribe({
+      next: (data) => (this.contacts = data),
+      error: (err) => console.log(err),
+    });
+  }
 }
