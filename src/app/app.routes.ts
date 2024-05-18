85d8176f3e6e2c0c87c124a4e07bd91d812d8a78
jottave <joao_victorflamengo10@outlook.com>
2024-05-17 23:54:34 -0300

    configure routes children

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,21 @@
 import { Routes } from '@angular/router';
+import { AddContactComponent } from './pages/home/add-contact/add-contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
-    loadComponent: () =>
-      import('./pages/home/home.component').then((c) => c.HomeComponent),
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'add',
+        component: AddContactComponent,
+      },
+    ],
   },
   {
     path: 'register',
