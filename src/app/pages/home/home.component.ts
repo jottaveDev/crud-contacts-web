b35f853d60b908f269c8f741b9526b1131b54e19
jottave <joao_victorflamengo10@outlook.com>
2024-05-20 10:42:38 -0300

    create methods add, edit and delete for contacts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { Contact } from '../../models/contact';
@@ -13,13 +14,35 @@ import { ContactsService } from '../../services/contacts/contacts.service';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
-  constructor(private contactsService: ContactsService) {}
+  constructor(
+    private contactsService: ContactsService,
+    private router: Router
+  ) {}
 
   contacts: Contact[] = [];
 
   ngOnInit() {
     this.contactsService.getContacts().subscribe({
       next: (data) => (this.contacts = data),
+      error: (err) => console.log(err),
+    });
+  }
+
+  addContact() {
+    this.router.navigate(['/home/add']);
+  }
+
+  editContact(contact: Contact) {
+    this.router.navigate([`/home/edit/${contact.id}`], {
+      state: { data: contact },
+    });
+  }
+
+  deleteContact(id: string) {
+    this.contactsService.deleteContact(id).subscribe({
+      next: () => {
+        this.contacts = this.contacts.filter((contact) => contact.id !== id);
+      },
       error: (err) => console.log(err),
     });
   }
