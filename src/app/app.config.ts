a124b5ce04139ab2e73b031abcbd1d9ed354ba7f
jottave <joao_victorflamengo10@outlook.com>
2024-05-20 10:41:31 -0300

    add imports modules browser global

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,18 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule)],
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      BrowserModule,
+      BrowserAnimationsModule,
+      HttpClientModule
+    ),
+  ],
 };
